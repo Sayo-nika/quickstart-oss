5d12452a1578fcd774568c09c8e3941377062a52
sr229 <aguilarralph13@gmail.com>
2018-06-22 22:20:31 +0800

    fix some stuff

--- setup.sh
+++ setup.sh
@@ -5,16 +5,18 @@ apikey = "6djJX9_ubYUj9aBpLggykL8kdnqmaiwdY8efJZuzqFGN"
 # This is intended for automating unzipping and putting the RPAs into the respect folder
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Checking for dir if it exists"
-if [ -d "mod" ]; then
+if [ -d "$cwd/mod" ]; then
   # dir exists, just unzip, and make new dir to add files
   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Directory exists. Creating game/ subdir and copying files over."
-  wget -d --header="Authorization: Bearer $(apiKey)" https://s3-api.us-geo.objectstorage.softlayer.net/filepub/ddlc_pkg.zip 
-  mkdir -p "mod/game"
+  wget -d --header="Authorization: Bearer $apiKey" https://s3-api.us-geo.objectstorage.softlayer.net/filepub/ddlc_pkg.zip 
+  mkdir -p "$cwd/mod/game"
   unzip ddlc_pkg.zip -d  mod/game 
+   exit 0
   else 
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Directory  does not exist. Creating dir and copying files over" 
     mkdir -p mod
-    wget -d --header="Authorization: Bearer $(apiKey)" https://s3-api.us-geo.objectstorage.softlayer.net/filepub/ddlc_pkg.zip 
+    wget -d --header="Authorization: Bearer $apiKey" https://s3-api.us-geo.objectstorage.softlayer.net/filepub/ddlc_pkg.zip 
     mkdir -p "mod/game"
-    unzip ddlc_pkg.zip -d  mod/game     
+    unzip ddlc_pkg.zip -d  mod/game    
+    exit 0 
 fi
