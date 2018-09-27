446b0cf57cb4bc51a4d8ce0f4dec16dce320bc91
Capuccino <enra@sayonika.moe>
2018-09-27 15:23:28 +0000

    flip conditions

--- inject_version.sh
+++ inject_version.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ ! -z TRAVIS_TAG ]; then
-  sed -i "s/config.version = \"0.1.2\"/config.version = \"$TRAVIS_COMMIT\"/g" mod/game/options.rpy;
+  sed -i "s/define config.version = \"0.1.2\"/define config.version = \"$TRAVIS_TAG\"/g" mod/game/options.rpy;
   cat mod/game/options.rpy | grep config.version;
 else
-  sed -i "s/define config.version = \"0.1.2\"/define config.version = \"$TRAVIS_TAG\"/g" mod/game/options.rpy;
+  sed -i "s/config.version = \"0.1.2\"/config.version = \"$TRAVIS_COMMIT\"/g" mod/game/options.rpy;
   cat mod/game/options.rpy | grep config.version;
 fi
