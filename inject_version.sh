60dbf2cda0de3778907340d2e2c3122eb359e7e4
Capuccino <enra@sayonika.moe>
2018-09-27 15:14:34 +0000

    I realized I did this wrong

--- inject_version.sh
+++ inject_version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ ! TRAVIS_TAG ]; then
-  sed -i "s/define config.version = \"0.1.2\"/define config.version = \"$TRAVIS_COMMIT\"/g" mod/game/options.rpy;
+if [ ! -z TRAVIS_TAG ]; then
+  sed -i "s/config.version = \"0.1.2\"/config.version = \"$TRAVIS_COMMIT\"/g" mod/game/options.rpy;
   cat mod/game/options.rpy | grep config.version;
 else
   sed -i "s/define config.version = \"0.1.2\"/define config.version = \"$TRAVIS_TAG\"/g" mod/game/options.rpy;
