de945d77f36179c7dc28a36117bebef489252c4e
Capuccino <enra@sayonika.moe>
2018-09-27 15:27:09 +0000

    real devs debug like this bitch

--- inject_version.sh
+++ inject_version.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 if [ ! -z TRAVIS_TAG ]; then
+  echo " ---> Tag exists, using tag."
   sed -i "s/define config.version = \"0.1.2\"/define config.version = \"$TRAVIS_TAG\"/g" mod/game/options.rpy;
   cat mod/game/options.rpy | grep config.version;
 else
+  echo " ---> Tag not exists, using hash."
   sed -i "s/config.version = \"0.1.2\"/config.version = \"$TRAVIS_COMMIT\"/g" mod/game/options.rpy;
   cat mod/game/options.rpy | grep config.version;
 fi
